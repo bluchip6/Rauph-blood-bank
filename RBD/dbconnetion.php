2bf5e743402738c87dec53c01abff3c3326cf8bd
bluchip6 <74207099+bluchip6@users.noreply.github.com>
2020-12-01 11:14:13 +0000

    Create dbconnetion.php

---
+++ RBD/dbconnetion.php
@@ -0,0 +1,11 @@
+<?php
+$_SERVER['HTTPS']= 'on';
+define('DB_NAME', $_SERVER['bloodbank']);
+define('DB_USER', $_SERVER['MainUser']);
+define('DB_PASSWORD', $_SERVER['12345678']);
+define('DB_HOST', $_SERVER['database-3.c7apijsslqfc.us-east-1.rds.amazonaws.com']);
+define('DB_TABLE', $_SERVER['bloodinfo']);
+define('DB_TABLE', $_SERVER['bloodrequest']);
+define('DB_TABLE', $_SERVER['hospitals']);
+define('DB_TABLE', $_SERVER['receivers']);
+?>
