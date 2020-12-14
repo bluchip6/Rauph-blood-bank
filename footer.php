9de47ad0b5515a0ad4b975da9ca760f255514d2f
Bluchip6 <mildseason3ees@gmail.com>
2020-12-14 12:00:57 +0000

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer text-center bg-light">
-	© Copyright <span id="demo"></span> <span class="brand">RAUPH Blood Donor. </span> DEVOPSSEC.
+	© Copyright <span id="demo"></span> <span class="brand">RAUPH BBMS. </span> DEVOPSSEC.
 	<script>
 		date = new Date();
     	document.getElementById("demo").innerHTML=date.getFullYear();
