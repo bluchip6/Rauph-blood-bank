4457a593298e07162b398b582de96300c434f66a
Bluchip6 <mildseason3ees@gmail.com>
2020-12-14 13:49:04 +0000

    fixdb

--- dbconnetion.php
+++ dbconnetion.php
@@ -3,7 +3,7 @@ $_SERVER['HTTPS']= 'on';
 define('DB_NAME', $_SERVER['bloodbank']);
 define('DB_USER', $_SERVER['RaufBBMS']);
 define('DB_PASSWORD', $_SERVER['RaufBBMS']);
-define('DB_HOST', $_SERVER['database-1.c7apijsslqfc.us-east-1.rds.amazonaws.com']);
+define('DB_HOST', $_SERVER['raufbbms.c7apijsslqfc.us-east-1.rds.amazonaws.com']);
 define('DB_TABLE', $_SERVER['bloodinfo']);
 define('DB_TABLE', $_SERVER['bloodrequest']);
 define('DB_TABLE', $_SERVER['hospitals']);
