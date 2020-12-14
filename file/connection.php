9d2507d43d827d042c98a92ce48a58399a09ec76
Bluchip6 <mildseason3ees@gmail.com>
2020-12-14 19:33:49 +0000

    db

--- file/connection.php
+++ file/connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "database-1.c7apijsslqfc.us-east-1.rds.amazonaws.com";
+$username = "abdul";
+$password = "12345678";
 $dbname = "bloodbank";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
